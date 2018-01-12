dd771cc648c5afd0f84720a608489d338e6ca7b9
BishoyHanna91 <bishoy@pdx.edu>
2018-01-12 15:25:16 -0800

    Added Badge In Route, Added UserSignInPage

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,9 +8,14 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/adminLogin', function(req, res) {
-  var path = require('path');
   var adminFilePath = path.resolve(__dirname, '..', 'views', 'adminLogin.html');
   res.sendFile(adminFilePath);
+});
+
+
+router.get('/badgein', function(req, res) {
+  var UserSignInPath = path.resolve(__dirname, '..', 'views', 'UserSignIn.html');
+  res.sendFile(UserSignInPath);
 });
 
 module.exports = router;
