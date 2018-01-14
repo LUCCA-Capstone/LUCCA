d4648fdef0de267038303bf07bacfef1aab06005
MikeOlivas <mio2@pdx.edu>
2018-01-14 02:26:38 -0800

    Revert "added adminLogin.html, adminLogin.css, and associated route"
    This reverts commit ae53e38e3670eb032ca18745d0c5eb77b474bffa.

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/adminLogin', function(req, res) {
-  var path = require('path');
-  var adminFilePath = path.resolve(__dirname, '..', 'views', 'adminLogin.html');
-  res.sendFile(adminFilePath);
-});
-
 module.exports = router;
