dc1e9b5b2aba40299e859d24e428c4e55b35e728
BishoyHanna91 <bishoy@pdx.edu>
2018-01-18 13:42:53 -0800

    Done some updates in order to fix the bugs for the project after removing the bugs files in the view folder

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
 });
 
 router.get('/adminLogin', function(req, res) {
