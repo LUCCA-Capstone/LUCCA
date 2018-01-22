1e04f8d9ee46d10eaec77e2808f0567326160024
Andy Wood <mongolsamurai@gmail.com>
2018-01-22 13:40:54 -0800

    Add a config file, and a module to handle its access

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var nunjucks = require('nunjucks');
 
+var config = require('./local_modules/config');
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
