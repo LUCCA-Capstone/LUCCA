c34df9036583696a18d811d9eb65c15362ee6854
huynguyencs <huy8@pdx.edu>
2018-01-24 18:01:54 -0800

    Modify HomePage.html using nunchuck blocks

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var path = require('path');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('HomePage.njk');
 });
 
