173f5062408f647c0b6d841bd0bde7216be3a83c
BishoyHanna91 <bishoy@pdx.edu>
2018-01-31 19:16:23 -0800

    add functionality to badge in page to get the badge number

--- routes/index.js
+++ routes/index.js
@@ -6,23 +6,23 @@ var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   res.render('HomePage.njk');
 });
 
-router.get('/adminLogin', function(req, res) {
+router.get('/adminLogin', function (req, res) {
   res.render('adminLogin.njk');
 });
 
-router.get('/badgein', function(req, res) {
+router.get('/badgein', function (req, res) {
   res.render('badgein.njk');
 });
 
-router.get('/registration/:badge', function(req, res) {
+router.get('/registration/:badge', function (req, res) {
   res.render('registration.njk');
 });
 
-router.post('/registration/:badge', jsonParser, function(req, res) {
+router.post('/registration/:badge', jsonParser, function (req, res) {
   if (!req.body) {
     //400 Bad Request
     return res.sendStatus(400);
@@ -32,7 +32,7 @@ router.post('/registration/:badge', jsonParser, function(req, res) {
   req.body.badge = req.params.badge;
 
   //Add mailing list signup as boolean (i.e. true instead of "on")
-  req.body.mailingList = (req.body.mailingList === undefined) ?  false : true; 
+  req.body.mailingList = (req.body.mailingList === undefined) ? false : true;
 
   /******************************************************************************
    * Should we add integrity checks here? (
@@ -40,11 +40,39 @@ router.post('/registration/:badge', jsonParser, function(req, res) {
    *******************************************************************************/
 
   //pass JSON object to createUser database method to add newly registered user
-  dbAPI.createUser(req.body).done(function(results){
+  dbAPI.createUser(req.body).done(function (results) {
     console.log(results);  //output results to console for now for dev purposes only
     //TODO: I will research reasonable ways to deal with errors and update later
   });
   res.redirect('/badgein');
+});
+
+router.post('/badgein', jsonParser, function (req, res) {
+  if (!req.body) {
+    return res.sendStatus(400);
+  }
+
+  var BadgeNumber = req.body.badgeNumber;
+
+  dbAPI.validateUser(BadgeNumber).then(function (result) {
+    console.log('BN = ' + BadgeNumber);
+
+    // this checks if the badge number is not in the database
+    if (result == undefined) {
+      // go to the registration page
+      res.redirect('/registration/' + BadgeNumber);
+    }
+
+    else {
+      console.log('You logged in succesfully');
+      console.log(BadgeNumber);
+
+      // popup message will apear here 
+      res.redirect('/badgein');
+    }
+
+  });
+
 });
 
 module.exports = router;
