fef218bbbeb3fce988395bbf11429abd3c3a0dde
Daniel Eynis <daniel.eynis@gmail.com>
2018-02-04 14:59:09 -0800

    This commit adds DB functionality to manipulate station information

--- database/models/index.js
+++ database/models/index.js
@@ -1,12 +1,12 @@
 'use strict';
 
-var fs        = require('fs');
-var path      = require('path');
+var fs = require('fs');
+var path = require('path');
 var Sequelize = require('sequelize');
-var basename  = path.basename(__filename);
-var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config/config.json')[env];
-var db        = {};
+var basename = path.basename(__filename);
+var env = process.env.NODE_ENV || 'development';
+var config = require(__dirname + '/../config/config.json')[env];
+var db = {};
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
@@ -33,14 +33,4 @@ Object.keys(db).forEach(modelName => {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-/*
-sequelize
-    .authenticate()
-    .then(() => {
-    console.log('Connection has been established successfully.');
-})
-.catch(err => {
-    console.error('Unable to connect to the database:', err);
-});
-*/
 module.exports = db;
--- database/models/user.js
+++ database/models/user.js
@@ -1,97 +1,97 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var user = sequelize.define('user', {
-      badge:
-          {
-              type: DataTypes.STRING,
-              primaryKey: true,
-              allowNull: false
-          },
-      first:
-          {
-              type: DataTypes.STRING,
-              allowNull: false
-          },
-      last:
-          {
-              type: DataTypes.STRING,
-              allowNull: false
-          },
-      email:
-          {
-              type: DataTypes.STRING,
-              allowNull: false
-          },
-      phone:
-          {
-              type: DataTypes.STRING,
-              allowNull: false
-          },
-      signature:
-          {
-              type: DataTypes.STRING,
-              allowNull: false
-          },
-      ecSignature:
-          {
-              type: DataTypes.STRING,
-              defaultValue: false
-          },
-      ecName:
-          {
-              type: DataTypes.STRING,
-              allowNull: false
-          },
-      ecRel:
-          {
-              type: DataTypes.STRING,
-              allowNull: false
-          },
-      ecPhone:
-          {
-              type: DataTypes.STRING,
-              allowNull: false
-          },
-      status:
-          {
-              type: DataTypes.STRING,
-              defaultValue: 'User',
-              allowNull: false
-          },
-      confirmation:
-          {
-            type: DataTypes.BOOLEAN,
-            defaultValue: false
-          },
-      password:
-          {
-              type: DataTypes.STRING,
-              defaultValue: null
-          },
-      mailingList:
-          {
-            type: DataTypes.BOOLEAN,
-            defaultValue: false
-          },
-      createdAt:
-          {
-              type: DataTypes.DATE,
-              allowNull: false
-          },
-      updatedAt:
-          {
-              type: DataTypes.DATE,
-              allowNull: false
-          }
+    badge:
+      {
+        type: DataTypes.STRING,
+        primaryKey: true,
+        allowNull: false
+      },
+    first:
+      {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+    last:
+      {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+    email:
+      {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+    phone:
+      {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+    signature:
+      {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+    ecSignature:
+      {
+        type: DataTypes.STRING,
+        defaultValue: false
+      },
+    ecName:
+      {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+    ecRel:
+      {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+    ecPhone:
+      {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+    status:
+      {
+        type: DataTypes.STRING,
+        defaultValue: 'User',
+        allowNull: false
+      },
+    confirmation:
+      {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+      },
+    password:
+      {
+        type: DataTypes.STRING,
+        defaultValue: null
+      },
+    mailingList:
+      {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+      },
+    createdAt:
+      {
+        type: DataTypes.DATE,
+        allowNull: false
+      },
+    updatedAt:
+      {
+        type: DataTypes.DATE,
+        allowNull: false
+      }
   });
 
-    user.associate = (models) => {
-        //add associations
-        user.hasMany(models.privileges, {
-            foreignKey: 'badge',
-            onDelete: 'CASCADE'
-        });
-    };
+  user.associate = (models) => {
+    //add associations
+    user.hasMany(models.privileges, {
+      foreignKey: 'badge',
+      onDelete: 'CASCADE'
+    });
+  };
 
   return user;
 };
