5c20a1b9764fbaeb4b10ea48c4b7eb96d6255bf2
MikeOlivas <mio2@pdx.edu>
2018-02-11 16:14:53 -0800

    added station management with get and post routes
    added stationManagement tests and removed extra padding above navbar
    
    Travis test failures
    
    updating package-lock file for passport module
    
    each row now has update and delete button and checkbox
    
    commenting out sort until database is avail to Travis
    
    updating comments

--- routes/index.js
+++ routes/index.js
@@ -44,10 +44,17 @@ module.exports = function (passport) {
     res.render('badgein.njk');
   });
 
+  /* GET registration page. Should be directed here from /badgein if/when the
+   *     user badging in is not yet registered. Renders RISK liabilty form data
+   *     for user to fill out. 
+   * *******************************************************************************************/
   router.get('/registration/:badge', function (req, res) {
     res.render('registration.njk');
   });
 
+  /* POST registration page. Uses form data to create a new user in the database.
+   *      temporarily outputs the results of dbAPI method to console until alerts can be added.
+   * ********************************************************************************************/
   router.post('/registration/:badge', jsonParser, function (req, res) {
     if (!req.body) {
       //400 Bad Request
@@ -59,11 +66,6 @@ module.exports = function (passport) {
 
     //Add mailing list signup as boolean (i.e. true instead of "on")
     req.body.mailingList = (req.body.mailingList === undefined) ? false : true;
-
-    /******************************************************************************
-     * Should we add integrity checks here? (
-     * e.g. the signature field must match the concatinated first name + last name
-     *******************************************************************************/
 
     //pass JSON object to createUser database method to add newly registered user
     dbAPI.createUser(req.body).done(function (results) {
@@ -125,6 +127,52 @@ module.exports = function (passport) {
       console.log(ret.dataValues);
       res.render('userManagement.njk', { obj: [ret.dataValues] });
     });
+  });
+
+  /*GET stationManagment route will render a table of all stations that match filter parameter.
+   *    If filter == "registered", only registered stations will be displayed (i.e. registered = true)
+   *    If filter == "unregistered", only unregistered stations will be displayed (i.e. registered = false)
+   *    Otherwise, all stations will be displayed sorted with unregistered stations first.
+   *********************************************************************************************************/
+  router.get('/stationManagement/:filter', jsonParser, function (req, res, next) {
+    //set filter based on query parameter passed in
+    var filter = req.params.filter;
+    filter = (filter === "registered") ? true : (filter === "unregistered") ? false : undefined;
+
+    //get results from database that match filter
+    dbAPI.getStations(filter).then(function(ret) {
+      //display ordered with unregistered stations first
+      /* UNCOMMENT THIS CODE WHEN TRAVIS HAS ACCESS TO A DATABASE SO RESULTS ARE SORTED
+       * ret.sort(function(a,b) {
+       *   return (a.registered === b.registered) ? 0 : a.registered ? 1 : -1;
+       * });
+      */
+      res.render('stationManagement.njk', {obj: ret});
+    });
+  });
+
+  /*POST stationManagement serves two purposes: if user req.body JSON contains a "delete" element,
+   *     then the user pushed a "delete" button and the station referenced by "sId" element is deleted from tables.
+   *     Otherwise, the station identified by "sId" is updated to values in JSON body and registered = true.
+   ***************************************************************************************************************/
+  router.post('/stationManagement/:filter', jsonParser, function (req, res) {
+    if (!req.body) {
+      //400 Bad Request
+      return res.sendStatus(400);
+    }
+
+    if(req.body.delete === "true") {
+      dbAPI.deleteStation(req.body.sId).then(function(ret) {
+        res.redirect('/stationManagement/' + req.params.filter);
+      });
+    } else {
+      //convert registered string to a boolean & new date before updating in tables
+      req.body.registered = (req.body.registered === "true") ? true : false;
+      req.body.updatedAt = new Date();
+      dbAPI.modifyStation(req.body.sId, req.body).then(function (ret) {
+        res.redirect('/stationManagement/' + req.params.filter);
+      });
+    }
   });
 
   return router;
