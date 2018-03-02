bb0154670e8bd23e6bb5371c268c51d0141a2326
Bryan Mikkelson <bsm5@pdx.edu>
2018-03-02 12:03:15 -0800

    --Moved DB configs to lucca.conf

--- database/models/index.js
+++ database/models/index.js
@@ -4,30 +4,40 @@ var fs = require('fs');
 var path = require('path');
 var Sequelize = require('sequelize');
 var basename = path.basename(__filename);
-var env = process.env.NODE_ENV || 'development';
-var config = require(__dirname + '/../config/config.json')[env];
-var db = {};
+var config = require(__dirname + '/../../local_modules/config');
+var user = config.get('Database', 'username');
+var database = config.get('Database', 'database');
+var password = config.get('Database', 'password');
+var loggingSetting = config.get('Database', 'logging');
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+if(loggingSetting === 'false')
+  loggingSetting = false;
+else
+  loggingSetting = true;
+
+var configurations = {
+  host: config.get('Database', 'host'),
+  dialect: config.get('Database', 'dialect'),
+  port: config.get('Database', 'port'),
+  logging: loggingSetting
+};
+var db = {};
+var sequelize = new Sequelize(database, user, password, configurations);
 
 fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    var model = sequelize['import'](path.join(__dirname, file));
-    db[model.name] = model;
-  });
+    .readdirSync(__dirname)
+    .filter(file => {
+  return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+})
+.forEach(file => {
+  var model = sequelize['import'](path.join(__dirname, file));
+db[model.name] = model;
+});
 
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+  db[modelName].associate(db);
+}
 });
 
 db.sequelize = sequelize;
