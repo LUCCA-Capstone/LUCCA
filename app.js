12684b889cbf7c980f57224821e9b413e5d76b59
Daniel Eynis <daniel.eynis@gmail.com>
2018-03-17 19:29:56 -0700

    Removed some HTML and increased session cookie expiration to 1hr

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
 /******************************************************************/
 
 var express = require('express');
-var path = require('path');
+var path = require('path'); 
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
@@ -39,7 +39,7 @@ app.set('view engine', 'nunjucks');
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(cookieParser('my_secret'));
-app.use(session({ secret: 'my_secret_session_key', cookie: { maxAge: 600000 } }));
+app.use(session({ secret: 'my_secret_session_key', cookie: { maxAge: 3600000 } }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(passport.initialize());
